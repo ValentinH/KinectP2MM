1860c99aaa6fb49a6ed42f509f2617c92f970ac9
cmercier <clemmercier@hotmail.com>
2013-11-01 03:40:14 +0100

    upgrade of config editor

--- KinectP2MM/Configuration.xaml.cs
+++ KinectP2MM/Configuration.xaml.cs
@@ -26,12 +26,13 @@ namespace KinectP2MM
             this.parentWindow = parentWindow;
         }
 
-        private void openFileChooser()
+        private String openFileChooser()
         {
             // Create OpenFileDialog 
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
 
             // Set filter for file extension and default file extension 
+            dlg.InitialDirectory = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location);
             dlg.Filter = "Images (*.png;*.jpg)|*.png;*.jpg";
 
 
@@ -44,27 +45,63 @@ namespace KinectP2MM
             {
                 // Open document 
                 String filename = dlg.FileName;
+                return filename;
             }
+            else
+                return null;
         }
 
         private void RightHandButton_Click(object sender, RoutedEventArgs e)
         {
-            openFileChooser();
+            String filePath = openFileChooser();
+            if (filePath != null)
+            {
+                RightHandTextBox.Text = filePath;
+            }
         }
 
         private void LeftHandButton_Click(object sender, RoutedEventArgs e)
         {
-            openFileChooser();
+            String filePath = openFileChooser();
+            if (filePath != null)
+            {
+                LeftHandTextBox.Text = filePath;
+            }
         }
 
         private void RightHandGripButton_Click(object sender, RoutedEventArgs e)
         {
-            openFileChooser();
+            String filePath = openFileChooser();
+            if (filePath != null)
+            {
+                RightHandGripTextBox.Text = filePath;
+            }
         }
 
         private void LeftHandGripButton_Click(object sender, RoutedEventArgs e)
         {
-            openFileChooser();
+            String filePath = openFileChooser();
+            if (filePath != null)
+            {
+                LeftHandGripTextBox.Text = filePath;
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            if (LeftHandTextBox.Text != String.Empty)
+                Properties.Settings.Default.left_hand = LeftHandTextBox.Text;
+
+            if (RightHandTextBox.Text != String.Empty)
+                Properties.Settings.Default.right_hand = RightHandTextBox.Text;
+
+            if (LeftHandGripTextBox.Text != String.Empty)
+                Properties.Settings.Default.left_hand_grip = LeftHandGripTextBox.Text;
+
+            if (RightHandGripTextBox.Text != String.Empty)
+                Properties.Settings.Default.right_hand_grip = RightHandGripTextBox.Text;
+
+            Properties.Settings.Default.Save();
         }
     }
 }
